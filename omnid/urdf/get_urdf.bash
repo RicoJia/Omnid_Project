14dbc60339c1474f2ed5c3d0e3f7ba333ec63ef6
RicoJia <ruotongjia2020@u.northwestern.edu>
2020-11-08 10:44:07 -0600

    moveit can now read urdf.xacro, fixed collision and home position. However it still does not work directly with the 3 omnid group

--- omnid/urdf/get_urdf.bash
+++ omnid/urdf/get_urdf.bash
@@ -1,4 +1,3 @@
-rosrun xacro xacro --inorder -o delta_robot.urdf delta_robot_moveit.urdf.xacro R_base:=0.18 h_base:=0.009525 M_base:=2.5 R_platform:=0.062 h_platform:=0.01 M_platform:=0.5 L_lower:=0.200 L_upper:=0.368 base_offset:=0.046375
 rosrun xacro xacro --inorder -o delta_robot_pybullet.urdf delta_robot_pybullet.urdf.xacro R_base:=0.18 h_base:=0.009525 M_base:=2.5 R_platform:=0.062 h_platform:=0.01 M_platform:=0.5 L_lower:=0.200 L_upper:=0.368 base_offset:=0.046375
 rosrun xacro xacro --inorder -o disk.urdf disk.urdf.xacro R_disk:=1.0 h_disk:=0.1 M_disk:=50.0
 
